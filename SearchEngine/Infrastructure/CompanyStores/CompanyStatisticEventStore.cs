6e8f67a11c5d5c1a9d2da7313acbdf9ec167397f
Ja-Rek <dto.jarek@gmail.com>
2019-07-20 03:37:12 +0200

    Add company stores module

---
+++ SearchEngine/Infrastructure/CompanyStores/CompanyStatisticEventStore.cs
@@ -0,0 +1,33 @@
+using AutoMapper;
+using Aveneo.Common.Domain.Events;
+using Aveneo.SearchEngine.Domain.Companies;
+using Aveneo.SearchEngine.Domain.Statistics;
+using Aveneo.SearchEngine.Infrastructure.Data;
+using NHibernate;
+
+namespace Aveneo.SearchEngine.Infrastructure.CompanyStores
+{
+    internal class CompanyStatisticEventStore<TCompanyStatisticEvent> : ICompanyStatisticEventStore<TCompanyStatisticEvent>
+        where TCompanyStatisticEvent : StatisticEvent, IEvent
+    {
+        private readonly ISession session;
+
+        public CompanyStatisticEventStore(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void Store(TCompanyStatisticEvent @event)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TCompanyStatisticEvent, CompanyStatisticData>());
+
+            var companyStatisticData = config.CreateMapper().Map<CompanyStatisticData>(@event);
+
+            using (var transaction = session.BeginTransaction())
+            {
+                session.Save(companyStatisticData);
+                transaction.Commit();
+            }
+        }
+    }
+}
