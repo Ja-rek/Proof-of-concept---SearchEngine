39a600574c3028961fb9a711b09dac766c9ffd4e
Ja-Rek <dto.jarek@gmail.com>
2019-07-21 15:51:41 +0200

    Add session factory

---
+++ Common/Infrastructure/Persistence/SessionFactory.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Tool.hbm2ddl;
+using System;
+
+namespace Aveneo.Common.Infrastructure.Persistence
+{
+    public static class SessionFactory
+    {
+        public static ISession Session(Action<MappingConfiguration> mapping, bool createDb = false)
+        {
+            var mySql = "server=localhost;User Id=root; password=Password; database=Aveneo;";
+
+            Action<NHibernate.Cfg.Configuration> shemaConfig = (cfg) => 
+            { 
+                if (createDb) new SchemaExport(cfg).Create(true, createDb); 
+                new SchemaUpdate(cfg).Execute(true, true); 
+            };
+
+            return Fluently.Configure()
+                .Database(MySQLConfiguration.Standard.ConnectionString(mySql).ShowSql())
+                .Mappings(mapping)
+                .ExposeConfiguration(shemaConfig)
+                .BuildSessionFactory()
+                .OpenSession();
+        }
+    }
+}
